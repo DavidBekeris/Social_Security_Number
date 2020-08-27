3e83fbab115ece91d4271245bafb600552cb6110
David <david-_-bkrs@hotmail.com>
2020-08-27 10:53:11 +0200

    C# Console App
    Using social security number able to retrieve age and gender of the person.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Social_Security_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
