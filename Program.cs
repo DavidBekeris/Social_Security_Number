914913de63edd5fc42c0dc4aa648c444efb4ec6e
David <david-_-bkrs@hotmail.com>
2020-08-28 11:47:22 +0200

    Done for now
    Some changes, functional

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using static System.Console;
 
 namespace Social_Security_Number
 {
@@ -11,18 +13,26 @@ namespace Social_Security_Number
 
             string socialSecurityNumber = Console.ReadLine();
 
-            string gender = "female";
-            int age = 0;
+            string gender;
 
             // Chooses the slot from social security number to use to determine what gender the person is
             //string genderNumber = socialSecurityNumber.Substring(9, 1);
 
             int genderNumber = int.Parse(socialSecurityNumber.Substring(socialSecurityNumber.Length - 2, 1));
 
-            gender = genderNumber % 2 != 0 ? "female" : "male";
+            gender = genderNumber % 2 != 0 ? "male" : "female";
 
             //int ageNumber = int.Parse(socialSecurityNumber.Substring(socialSecurityNumber.Length - 11, 6));
             string ageNumber = socialSecurityNumber.Substring(socialSecurityNumber.Length - 11, 6);
+
+            DateTime birthDate = DateTime.ParseExact(ageNumber, "yyMMdd", CultureInfo.InvariantCulture);
+
+            int age = DateTime.Now.Year - birthDate.Year;
+
+            if ((birthDate.Month > DateTime.Now.Month) || (birthDate.Month == DateTime.Now.Month && birthDate.Day > DateTime.Now.Day))
+            {
+                age--;
+            }
 
             Console.WriteLine("You're a {0} and {1} years old.", gender, age);
             //Console.WriteLine($"This is a {gender}, and the age is {age}");
